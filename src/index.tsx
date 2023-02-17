1e58fb36de242e4a2a1390fdc6d37e3750bae93c
Rafael Ito <rito@briza.com>
2023-02-17 20:45:48 -0300

    react start

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import FirstComponent from './components/FirstComponent'
+
+ReactDOM.render(
+    <div>
+      <h1>Hello, Welcome to React and TypeScript</h1>
+      <FirstComponent/>
+    </div>,
+    document.getElementById("root")
+);
