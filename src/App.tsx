e606ec96878cf0ce7352d6c05a981b643bdfc748
Rafael Ito <rito@briza.com>
2023-02-18 03:34:09 -0300

    remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import { SUGGESTIONS } from './countries';
 import './style.css';
