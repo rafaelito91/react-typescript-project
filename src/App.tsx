eb9ce688a82980a49a8e94da9c4b73ec6bba0f71
Rafael Ito <rito@briza.com>
2023-02-17 23:25:08 -0300

    made it work somehow

---
+++ src/App.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import { COUNTRIES } from './countries';
+import './style.css';
+import { Tag, WithContext as ReactTags } from 'react-tag-input';
+
+const suggestions = COUNTRIES.map(country => {
+  return {
+    id: country,
+    text: country
+  };
+});
+
+const KeyCodes = {
+  comma: 188,
+  enter: 13
+};
+
+const delimiters = [KeyCodes.comma, KeyCodes.enter];
+
+export const App = () => {
+  const [tags, setTags] = React.useState([
+    { id: 'Thailand', text: 'Thailand' },
+    { id: 'India', text: 'India' },
+    { id: 'Vietnam', text: 'Vietnam' },
+    { id: 'Turkey', text: 'Turkey' }
+  ]);
+
+  const handleDelete = (i: number) => {
+    setTags(tags.filter((tag, index) => index !== i));
+  };
+
+  const handleAddition = (tag: Tag) => {
+    setTags([...tags, tag]);
+  };
+
+  const handleDrag = (tag: Tag, currPos: number, newPos: number) => {
+    const newTags = tags.slice();
+
+    newTags.splice(currPos, 1);
+    newTags.splice(newPos, 0, tag);
+
+    // re-render
+    setTags(newTags);
+  };
+
+  const handleTagClick = (index: number) => {
+    console.log('The tag at index ' + index + ' was clicked');
+  };
+
+  return (
+    <div className="app">
+      <h1> React Tags Example </h1>
+      <div>
+        <ReactTags
+          tags={tags}
+          suggestions={suggestions}
+          delimiters={delimiters}
+          handleDelete={handleDelete}
+          handleAddition={handleAddition}
+          handleDrag={handleDrag}
+          handleTagClick={handleTagClick}
+          inputFieldPosition="top"
+          autocomplete
+        />
+      </div>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById('root'));
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import FirstComponent from './components/FirstComponent'
-import UserComponent from './components/UserComponent'
-ReactDOM.render(
-    <div>
-      <h1>Hello, Welcome to React and TypeScript</h1>
-      <FirstComponent/>
-      <UserComponent name="John Doe" age={26} address="87 Summer St, Boston, MA 02110" dob={new Date()} />
-    </div>,
-    document.getElementById("root")
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import {App} from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
 );
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
