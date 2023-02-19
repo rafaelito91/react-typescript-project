ee001976777ef5ae82548ace0bb99e1cc077a0c3
Rafael Ito <rafaelito91@gmail.com>
2023-02-19 18:35:36 -0300

    new tags

--- src/suggestionsValues.ts
+++ src/suggestionsValues.ts
@@ -1,15 +1,16 @@
 import { Tag } from "react-tag-input"
 import { removeEmphasis } from "./App"
 
-export type TagType = 'picture-quality' | 'character-quality' | 'background-quality' | 'effect' | 'positioning' | 'pose'
+export type TagType = 'picture-quality' | 'character-quality' | 'background-quality' | 'effect' | 'positioning' | 'pose' | 'style'
 
 export const tagTypeMapping: Record<TagType, [string, string[]]> = {
  pose: ['blue', ['sitting', 'looking at viewer']],
  positioning: ['green', []],
- effect: ['pink', []],
+ effect: ['pink', ['semi-silhouette light']],
+ style: ['purple', ['redshift style', 'analog style', 'mdjrny-v4 style', 'dreamlikeart style', 'studio ghibli style', 'copeseethemald style']],
  "background-quality": ['orange', ["detailed background"]],
- "character-quality": ['yellow', ["subsurface skin scattering","shiny skin","beautiful detailed eyes","realistic", "photorealistic", "well-lit face","smooth","glowing face","highly detailed facial features",]],
- "picture-quality": ['red', ["RAW", "analog", "Nikon Z 85mm", "award winning glamour photograph","sharp focus", "digital render", "professional", "4k", "artstation", "artgerm", "octane render", "highres","ultra realistic", "photorealism", "photography", "8k", "uhd", "photography","octane render", "photorealistic", "realistic", "post-processing", "max detail", "roughness", "real life", "octane render","dust particle paint explosion","high detail", "sharp focus", "aesthetic", "extremely detailed", "stamp", "masterpiece"]],
+ "character-quality": ['yellow', ["subsurface skin scattering","shiny skin","beautiful detailed eyes","realistic", "photorealistic", "well-lit face","smooth","glowing face","highly detailed facial features", "detailed facial features"]],
+ "picture-quality": ['red', ["RAW", "analog", "Nikon Z 85mm", "award winning glamour photograph","sharp focus", "digital render", "professional", "4k", "artstation", "artgerm", "octane render", "highres","ultra realistic", "photorealism", "photography", "8k", "uhd", "photography","octane render", "photorealistic", "realistic", "post-processing", "max detail", "roughness", "real life", "octane render","dust particle paint explosion","high detail", "sharp focus", "aesthetic", "extremely detailed", "stamp", "masterpiece", "highly detailed", "intricate details", "best quality"]],
 }
 
 export const tagTypes:TagType[] = Object.keys(tagTypeMapping) as TagType[]
